00d715abee8efe5da1e14e61215f612587c50096
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2024-02-08 13:02:07 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="container mx-auto ">
+    <HelloWorld msg="You did it!" />
+  </header>
+</template>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center gap-8">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h1 class="text-8xl font-bold">{{ msg }}</h1>
+    <p class="text-xl">
+      You've successfully created a project with
+      <a
+        href="https://vitejs.dev/"
+        target="_blank"
+        rel="noopener"
+        class="font-medium text-lime-600 underline hover:no-underline"
+        >Vite</a
+      >
+      +
+      <a
+        href="https://vuejs.org/"
+        target="_blank"
+        rel="noopener"
+        class="font-medium text-lime-600 underline hover:no-underline"
+        >Vue 3</a
+      >.
+    </p>
+  </div>
+</template>
