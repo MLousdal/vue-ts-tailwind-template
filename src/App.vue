207d663304dcc459e06ac0eae717d0b5bb00c3c6
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2024-11-01 10:26:51 +0100

    Updated prettier

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+	import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <header class="container mx-auto ">
-    <HelloWorld msg="You did it!" />
-  </header>
+	<header class="container mx-auto">
+		<HelloWorld msg="You did it!" />
+	</header>
 </template>
 
-<style>
-</style>
+<style></style>
